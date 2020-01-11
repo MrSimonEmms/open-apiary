89282d2a452391827371755177498e1fd020f92c
Simon Emms <simon@simonemms.com>
2020-01-11 21:15:44 +0000

    fix(server): set the dev build to wait until it's ready before buildling

--- server/nuxt/nuxt.controller.ts
+++ server/nuxt/nuxt.controller.ts
@@ -20,7 +20,10 @@ export default class NuxtController {
   constructor() {
     if (process.env.IS_NUXT_ENABLED) {
       this.nuxt = new Nuxt(config);
-      new Builder(this.nuxt).build();
+      this.nuxt.ready()
+        .then(() => {
+          new Builder(this.nuxt).build();
+        });
     } else if (process.env.IS_NUXT_DISABLED !== 'true') {
       config.dev = false;
       config.buildModules = []; // These are running when they shouldn't be
