a181bdab2e211953b0723c8669ec2f73c571b90c
Simon Emms <simon@simonemms.com>
2020-01-13 11:10:08 +0000

    feat(settings): create settings page
    At the moment, this is just a stubbed endpoint. More later

---
+++ client/pages/settings/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div index
+</template>
+
+<script lang="ts">
+/**
+ * index
+ */
+
+/* Node modules */
+import { Vue, Component } from 'vue-property-decorator';
+
+/* Files */
+
+@Component
+export default class SettingsNestedPage extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
