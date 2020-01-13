3f6a8d3ed05046983d707863af712c4c80099239
Simon Emms <simon@simonemms.com>
2020-01-13 16:16:41 +0000

    feat(app-bar): create the base app bar
    At the moment, this is just a toggle for the nav drawer. This will have more things in future

--- client/plugins/components.ts
+++ client/plugins/components.ts
@@ -8,6 +8,7 @@
 import { Vue } from 'vue-property-decorator';
 
 /* Files */
+import AppBar from '../components/appBar.vue';
 import BaseLayout from '../components/baseLayout.vue';
 import NavDrawer from '../components/navDrawer.vue';
 import NavList from '../components/navList.vue';
@@ -17,6 +18,7 @@ const { Fragment } = require('vue-fragment');
 
 Vue.component('Fragment', Fragment); // This should be used sparingly
 
+Vue.component('oaAppBar', AppBar);
 Vue.component('oaBaseLayout', BaseLayout);
 Vue.component('oaNavDrawer', NavDrawer);
 Vue.component('oaNavList', NavList);
