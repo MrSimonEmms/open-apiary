13d3db68ee8b8961688be968f71e0d013e8fb192
Simon Emms <simon@simonemms.com>
2020-01-13 16:16:41 +0000

    feat(app-bar): create the base app bar
    At the moment, this is just a toggle for the nav drawer. This will have more things in future

---
+++ client/components/appBar.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  v-app-bar(
+    app
+    flat
+    color="transparent"
+  )
+    v-app-bar-nav-icon.d-md-none(
+      @click="drawer = !drawer"
+    )
+</template>
+
+<script lang="ts">
+/**
+ * appBar
+ */
+
+/* Node modules */
+import { Vue, Component } from 'vue-property-decorator';
+
+/* Files */
+
+@Component
+export default class AppBar extends Vue {
+  get drawer() : boolean | null {
+    return this.$store.getters['app/drawerDisplay'];
+  }
+
+  set drawer(state: boolean | null) {
+    this.$store.commit('app/setDrawerDisplay', state);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
