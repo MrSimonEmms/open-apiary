0e10f83d1d92cf7cce177234b283e625c2371ef7
Simon Emms <simon@simonemms.com>
2020-01-14 13:20:26 +0000

    fix(authentication): correctly handle a token validation error by sending to login page

--- client/middleware/isLoggedIn.ts
+++ client/middleware/isLoggedIn.ts
@@ -6,6 +6,7 @@
 
 /* Third-party modules */
 import { Middleware } from '@nuxt/types'; // eslint-disable-line import/no-unresolved
+import { Vue } from 'vue-property-decorator';
 
 /* Files */
 
@@ -14,16 +15,27 @@ const middleware : Middleware = async ({ redirect, route, store }) => {
 
   if (isLoggedIn) {
     /* Logged in, get the user data */
-    await store.dispatch('user/loadUser');
-  } else {
-    /* If we're not logged in, go to login page */
-    store.commit('user/logout');
-    store.commit('user/setRedirect', route);
+    try {
+      await store.dispatch('user/loadUser');
+      Vue.$log.info('User login token is valid');
 
-    redirect({
-      name: 'login',
-    });
+      return;
+    } catch (err) {
+      Vue.$log.info('User\'s login token invalid - logging out', {
+        err,
+      });
+    }
+  } else {
+    Vue.$log.info('User has no login token');
   }
+
+  /* If we get here, we're not logged in - log out */
+  store.commit('user/logout');
+  store.commit('user/setRedirect', route);
+
+  redirect({
+    name: 'login',
+  });
 };
 
 export default middleware;
