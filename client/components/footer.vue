15cb02ef6adeb83e4a12804a83345233b6491367
Simon Emms <simon@simonemms.com>
2020-01-17 09:02:59 +0000

    feat(footer): create footer and add to default layout

---
+++ client/components/footer.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  v-footer( absolute )
+    div footer
+</template>
+
+<script lang="ts">
+/**
+ * footer
+ */
+
+/* Node modules */
+import { Vue, Component } from 'vue-property-decorator';
+
+/* Files */
+
+@Component
+export default class Footer extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- client/plugins/components.ts
+++ client/plugins/components.ts
@@ -10,6 +10,7 @@ import { Vue } from 'vue-property-decorator';
 /* Files */
 import AppBar from '../components/appBar.vue';
 import BaseLayout from '../components/baseLayout.vue';
+import Footer from '../components/footer.vue';
 import NavDrawer from '../components/navDrawer.vue';
 import NavList from '../components/navList.vue';
 import NavListItem from '../components/navListItem.vue';
@@ -20,6 +21,7 @@ Vue.component('Fragment', Fragment); // This should be used sparingly
 
 Vue.component('oaAppBar', AppBar);
 Vue.component('oaBaseLayout', BaseLayout);
+Vue.component('oaFooter', Footer);
 Vue.component('oaNavDrawer', NavDrawer);
 Vue.component('oaNavList', NavList);
 Vue.component('oaNavListItem', NavListItem);
