3c06c9eef03b6623c662b2e5dd6eb2b859930019
Simon Emms <simon@simonemms.com>
2020-01-20 16:37:46 +0000

    feat(messages): add messages to the base layout and trigger when apiary saved

---
+++ client/interfaces/app.ts
@@ -0,0 +1,20 @@
+/**
+ * app
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+
+/* Files */
+
+export interface ISystemMsg {
+  msg: string;
+  top?: boolean;
+  bottom?: boolean;
+  left?: boolean;
+  right?: boolean;
+  timeout?: number;
+  multiLine?: boolean;
+  vertical?: boolean;
+}
--- client/store/app.ts
+++ client/store/app.ts
@@ -7,8 +7,15 @@
 /* Third-party modules */
 import { Vue } from 'vue-property-decorator';
 import { Module, Mutation, VuexModule } from 'vuex-module-decorators';
+import uuid from 'uuid';
 
 /* Files */
+import { ISystemMsg } from '../interfaces/app';
+
+interface ISystemMsgItem {
+  key: string;
+  message: ISystemMsg;
+}
 
 @Module({
   name: 'app',
@@ -19,6 +26,8 @@ export default class AppStore extends VuexModule {
   drawer: boolean | null = null;
 
   drawerMini: boolean = false;
+
+  systemMessage: ISystemMsgItem[] = [];
 
   title?: string;
 
@@ -45,6 +54,54 @@ export default class AppStore extends VuexModule {
     return this.title;
   }
 
+  get systemMessages() {
+    return this.systemMessage;
+  }
+
+  @Mutation
+  addSystemMessage(msg: ISystemMsg | string) {
+    const messages : ISystemMsgItem[] = [
+      ...this.systemMessage,
+    ];
+
+    /* Default options */
+    let message : ISystemMsg = {
+      msg: '',
+      top: false,
+      bottom: true,
+      left: false,
+      right: false,
+      timeout: 5000,
+      multiLine: false,
+      vertical: false,
+    };
+
+    if (typeof msg === 'string') {
+      message.msg = msg;
+    } else {
+      message = {
+        ...message,
+        ...msg,
+      };
+    }
+
+    messages.push({
+      message,
+      key: uuid.v4(),
+    });
+
+    Vue.set(this, 'systemMessage', messages);
+  }
+
+  @Mutation
+  removeSystemMessage(key: string) {
+    const messages : ISystemMsgItem[] = [
+      ...this.systemMessage,
+    ].filter((item) => item.key !== key);
+
+    Vue.set(this, 'systemMessage', messages);
+  }
+
   @Mutation
   setDrawerDisplay(state: boolean) {
     Vue.set(this, 'drawer', state);
@@ -61,7 +118,7 @@ export default class AppStore extends VuexModule {
   }
 
   @Mutation
-  setUUID(uuid: string) {
-    Vue.set(this, 'uuid', uuid);
+  setUUID(str: string) {
+    Vue.set(this, 'uuid', str);
   }
 }
