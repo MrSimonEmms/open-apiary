a0d98cb14cca2f479732301a3dcae6d374e8b53c
Simon Emms <simon@simonemms.com>
2020-02-14 12:11:14 +0000

    feat(docs): configure docs building and send to gitlab pages

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+/**
+ * config
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+
+/* Files */
+
+module.exports = {
+  title: 'Open Apiary',
+  description: 'An application to enable beekeepers to keep track of their records and maintain healthy apiaries.',
+  base: process.env.CONFIG_BASE,
+};
