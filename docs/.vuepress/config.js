9ba3203810d154d46a9b2d0a7da9b7f80bf3507b
Simon Emms <simon@simonemms.com>
2020-02-15 22:30:47 +0000

    chore(docs): add the sidebar/menu pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,4 +12,40 @@ module.exports = {
   title: 'Open Apiary',
   description: 'An application to enable beekeepers to keep track of their records and maintain healthy apiaries.',
   base: process.env.CONFIG_BASE,
+  themeConfig: {
+    nav: [{
+      text: 'Guide',
+      link: '/guide/',
+    }, {
+      text: 'Usage',
+      link: '/usage/',
+    }, {
+      text: 'Contributing',
+      link: '/contributing/',
+    }, {
+      text: 'Issues',
+      link: 'https://gitlab.com/MrSimonEmms/open-apiary/issues',
+    }, {
+      text: 'Source Code',
+      link: 'https://gitlab.com/mrsimonemms/open-apiary',
+    }],
+    sidebar: [{
+      title: 'Guide',
+      path: '/guide/',
+      children: [
+        '/guide/',
+        '/guide/getting-started',
+        '/guide/setup',
+        '/guide/deployment',
+      ],
+    }, {
+      title: 'Usage',
+      path: '/usage/',
+      children: [],
+    }, {
+      title: 'Contributing',
+      path: '/contributing/',
+      children: [],
+    }],
+  }
 };
