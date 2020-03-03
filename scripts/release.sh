24a8acdebafe9b1321057a3549007b017bcde6cd
Simon Emms <simon@simonemms.com>
2020-03-03 22:22:58 +0000

    fix(release): update the gitlab_release to use the dist folder

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,39 +4,65 @@
 
 set -e
 
-VERSION=$(cat VERSION)
+VERSION="v$(cat VERSION)"
 
-# Create the release
+echo "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/${VERSION}"
+
+# Check for existing release
 release=$(curl --location \
-  --fail \
-  --request POST "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases" \
   --header "Content-Type: application/json" \
   --header "Authorization: token ${GITHUB_DEPLOY_TOKEN}" \
-  --data-raw "{ \"tag_name\": \"${VERSION}\" }")
+  --request GET "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/tags/${VERSION}")
 
 releaseId=$(echo "${release}" | jq '.id')
 
-# Zip and upload the artifacts
-rm -f ./artifacts/*.zip
-cd ./artifacts
-for i in *
-do
-  echo "Zipping ${i}"
-  cd "${i}"
+if [ "$releaseId" = "null" ]; then
+  echo "No release - creating"
+  # Create the release
+  release=$(curl --location \
+    --fail \
+    --request POST "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases" \
+    --header "Content-Type: application/json" \
+    --header "Authorization: token ${GITHUB_DEPLOY_TOKEN}" \
+    --data-raw "{ \"tag_name\": \"${VERSION}\" }")
 
-  artifact="${VERSION}-${i}.zip"
+  releaseId=$(echo "${release}" | jq '.id')
+fi
 
-  zip -r "../${artifact}" .
-  cd ../
+# Zip and upload the dist
+echo "Zipping ${VERSION}"
 
-  # Uploading the artifact
+artifact="open-apiary-${VERSION}.zip"
+
+rm -Rf ./open-apiary
+cp -Rf ./dist ./open-apiary
+
+zip -r "./${artifact}" ./open-apiary
+
+# Check if asset currently exists, delete if it does
+assets=$(curl --location \
+  --fail \
+  --header "Content-Type: application/json" \
+  --header "Authorization: token ${GITHUB_DEPLOY_TOKEN}" \
+  "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/${releaseId}/assets")
+
+for asset in $(echo $assets | jq -rc 'map(.id) | .[]'); do
+  echo "Deleting ${asset}"
+
   curl --location \
     --fail \
-    --request POST "https://uploads.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/${releaseId}/assets?name=${artifact}" \
+    --header "Content-Type: application/json" \
     --header "Authorization: token ${GITHUB_DEPLOY_TOKEN}" \
-    --header "Content-Type: application/zip" \
-    --data-binary "@${PWD}/${artifact}"
+    --request DELETE "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/assets/${asset}"
 done
-cd ..
+
+## Uploading the artifact
+echo "Uploading artifact - ${artifact} to ${VERSION}"
+curl --location \
+  --fail \
+  --request POST "https://uploads.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/${releaseId}/assets?name=${artifact}" \
+  --header "Authorization: token ${GITHUB_DEPLOY_TOKEN}" \
+  --header "Content-Type: application/zip" \
+  --data-binary "@${PWD}/${artifact}"
 
 echo "Finished"
